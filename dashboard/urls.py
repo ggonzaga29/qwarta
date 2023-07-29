05f7834a2c5cea86e5a3d7a3c8954c2318590427
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2023-07-30 02:56:18 +0800

    feat: add create loan route

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,9 +10,13 @@ urlpatterns = [
     path("clients/", views.ClientsView.as_view(), name='clients'),
     path("loans/<int:loan_id>/", views.ViewLoanView.as_view(), name='loan'),
     path("loans/approve/<int:loan_id>/", views.ApproveView.as_view(), name="approve"),
+    path("loans/reject/<int:loan_id>/", views.RejectView.as_view(), name="reject"),
+    path("loans/disapprove/<int:loan_id>/", views.DisapproveView.as_view(), name="disapprove"),
     path("loans/delete/<int:loan_id>/", views.DeleteLoanView.as_view(), name="delete"),
     path("clients/edit/", views.EditProfileClient.as_view(), name="edit"),
     path("clients/<int:user_id>/loans/", views.ClientLoanView.as_view(), name="edit"),
+    # create loan resource
+    path("loans/create/", views.CreateLoanView.as_view(), name="create"),
     path("payments/", views.PaymentsView.as_view(), name="payments"),
 
 ]
