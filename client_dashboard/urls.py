b426557e300efe68483f60e3d5692012ece8056d
YAGOO (not real) <rosnuker@gmail.com>
2023-07-30 15:12:03 +0800

    add: apply loan

--- client_dashboard/urls.py
+++ client_dashboard/urls.py
@@ -6,4 +6,5 @@ app_name = "client_dashboard"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name='client_dashboard'),
+    path("loan/apply/<int:client_id>/", views.ApplyLoanView.as_view(), name="apply"),
 ]
